547745142311898b5e59ce08370881b33107788a
hansensa <hansensa48@craighead.school.nz>
2025-05-29 08:51:54 +1200

    Update 0-calculations.js

--- code/0-calculations.js
+++ code/0-calculations.js
@@ -2,4 +2,26 @@
 // #### ----- Calculations ----- ######
 // ####################################
 
-function calculations() {}
+function calculations() {
+    // Snippet 1
+    alert(8 + 2)
+    alert(8 - 2)
+    alert(8 * 2)
+    alert(10 / 4)
+
+    // Snippet 2
+    let firstNumber = 20
+    let secondNumber = 5
+
+    let result = firstNumber * secondNumber
+
+    alert(result)
+
+    // Snippet 3
+    firstNumber = prompt('Enter a number')
+    secondNumber = 2
+
+    result = first_number * second_number
+
+    alert(firstNumber + ' x ' + secondNumber + ' = ' + result)
+}
